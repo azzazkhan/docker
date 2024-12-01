812ec3136db03018043c02d60f9328b1fb4b3df7
Azzaz Khan <hello@azzaz.dev>
2024-12-01 16:13:26 +0500

    feat: Added Docker network creation and logs in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,23 @@
 export DEBIAN_FRONTEND=noninteractive
 set -eu
 
+# Create required docker networks (if not already created)
+docker network ls | grep -q traefik || docker network create traefik
+docker network ls | grep -q docker || docker network create docker
+
+echo "✅ Created required Docker networks"
+
 # Copy environment variables file if not already present
 if [[ ! -f .env ]]; then
-    echo "Creating environment configuration file for base containers"
-
     cp .env.example .env
     sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$(pwgen -cnsBv 20 1)/" .env
+
+    echo "✅ Created default environment configuration file"
 fi
 
 # Create access log for Traefik as Docker mount point
 if [[ ! -f volumes/traefik/access.log ]]; then
-    echo "Creating access log file for Traefik reverse proxy"
-
     touch volumes/traefik/access.log
+
+    echo "✅ Created Traefik access log file"
 fi
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,16 @@
 # Pull latest version of all images
 docker compose -f docker-compose.yml pull
 
+echo "✅ Pulled latest images for all services"
+
 # Start all services
 docker compose up -d
+
+echo "✅ Started all services"
 
 # Only auto-restart non-essential containers!
 docker compose -f docker-compose.yml restart mailpit &
 docker compose -f docker-compose.yml restart phpmyadmin &
 wait
+
+echo "✅ Restarted MailPit and PhpMyAdmin services"
