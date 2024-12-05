528d888a5f4c34e13927aad7224efb7b761ad38a
Azzaz Khan <hello@azzaz.dev>
2024-12-05 16:45:33 +0500

    feat: Added option in update script to start services in HTTP mode

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,19 +3,36 @@
 export DEBIAN_FRONTEND=noninteractive
 set -eu
 
+UNSECURE=false
+COMPOSE_FILE="docker-compose.yml"
+
+while getopts 'u' flag; do
+    case "${flag}" in
+        u) UNSECURE=true ;;
+        *) true
+    esac
+done
+
+if [ "$UNSECURE" = true ]; then
+    COMPOSE_FILE="docker-compose.http.yml"
+    echo "Using unsecure compose file"
+else
+    echo "Using secure compose file"
+fi
+
 # Pull latest version of all images
-docker compose -f docker-compose.yml pull
+docker compose -f "$COMPOSE_FILE" pull
 
 echo "✅ Pulled latest images for all services"
 
 # Start all services
-docker compose up -d
+docker compose -f "$COMPOSE_FILE" up -d
 
 echo "✅ Started all services"
 
 # Only auto-restart non-essential containers!
-docker compose -f docker-compose.yml restart mailpit &
-docker compose -f docker-compose.yml restart phpmyadmin &
+docker compose -f "$COMPOSE_FILE" restart mailpit &
+docker compose -f "$COMPOSE_FILE" restart phpmyadmin &
 wait
 
 echo "✅ Restarted MailPit and PhpMyAdmin services"
